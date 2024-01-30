a40a8cc7f65e83df1d12111e762bbe5b748959cb
unknown <uro123uzb@gmail.com>
2024-01-30 21:53:54 +0500

    changes

---
+++ script.js
@@ -0,0 +1,65 @@
+elItem2 = document.querySelector('.nav__item-two');
+elList2 = document.querySelector('.sublist-two');
+elItem2.addEventListener('click', function(){
+    elList2.classList.toggle('open');
+});
+elBtns = document.querySelector('.burger-btn');
+elContents = document.querySelector('.site-header__list');
+elBtns.addEventListener('click', function() {
+    elContents.classList.toggle('open1');
+});
+
+
+elBtn = document.querySelector('.button123');
+elContent = document.querySelector('.site-item-bottom');
+elBtn.addEventListener('click', function() {
+    elContent.classList.toggle('opens');
+    elBtn.classList.toggle('rotate');
+});
+
+
+elBtn1 = document.querySelector('.button1231');
+elContent1 = document.querySelector('.site-item-bottom1');
+elBtn1.addEventListener('click', function() {
+    elContent1.classList.toggle('opens');
+    elBtn1.classList.toggle('rotate');
+});
+
+
+elBtn2 = document.querySelector('.button1232');
+elContent2 = document.querySelector('.site-item-bottom2');
+elBtn2.addEventListener('click', function() {
+    elContent2.classList.toggle('opens');
+    elBtn2.classList.toggle('rotate');
+});
+
+
+elBtn3 = document.querySelector('.button1233');
+elContent3 = document.querySelector('.site-item-bottom3');
+elBtn3.addEventListener('click', function() {
+    elContent3.classList.toggle('opens');
+    elBtn3.classList.toggle('rotate');
+});
+
+
+elBtn4 = document.querySelector('.button1234');
+elContent4 = document.querySelector('.site-item-bottom4');
+elBtn4.addEventListener('click', function() {
+    elContent4.classList.toggle('opens');
+    elBtn4.classList.toggle('rotate');
+});
+
+
+elBtn5 = document.querySelector('.button1235');
+elContent5 = document.querySelector('.site-item-bottom5');
+elBtn5.addEventListener('click', function() {
+    elContent5.classList.toggle('opens');
+    elBtn5.classList.toggle('rotate');
+});
+
+elBtn6 = document.querySelector('.button1236');
+elContent6 = document.querySelector('.site-item-bottom6');
+elBtn6.addEventListener('click', function() {
+    elContent6.classList.toggle('opens');
+    elBtn6.classList.toggle('rotate');
+});
